7b9403cc7ad86cdb1276dff416175832b6870d60
Sasha Rosales <oleksandr.rosales@gmail.com>
2023-11-18 13:21:47 +0200

    Решил задачу с факториалами

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,36 @@
+// function factorial(n) {
+//   let res = n;
+
+//   if (n === 0) {
+//     return 1;
+//   }
+
+//   for (let i = 1; i <= n; i++) {
+//     if (n - i !== 0) {
+//       res = res * (n - i);
+//     }
+//   }
+//   return res;
+// }
+
+
 function factorial(n) {
-  // ваш код...
+  if (n === 0) {
+    return 1;
+  }
+
+  let res = 1;
+
+  for (let i = n; i > 1; i--) {
+    res = res * i;
+  }
+
+
+
+  return res;
 }
+
+factorial(0); // 1
+factorial(1); // 1
+factorial(3); // 6
+factorial(5);
